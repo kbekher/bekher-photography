5c387fe9bd109a274408bd80aa05d9dc042558d4
Kristina Bekher <krbekher@gmail.com>
2025-03-31 21:42:22 +0200

    try less transitions

--- src/components/PageTransition.tsx
+++ src/components/PageTransition.tsx
@@ -3,7 +3,7 @@
 import { AnimatePresence } from "framer-motion";
 import { usePathname } from "next/navigation";
 import React from "react";
-import TransitionLayer from "./TransitionLayer";
+// import TransitionLayer from "./TransitionLayer";
 
 interface Props {
   children: React.ReactNode;
@@ -14,7 +14,7 @@ const PageTransition: React.FC<Props> = ({ children }) => {
 
   return (
     <AnimatePresence mode="wait" initial={false}>
-      <TransitionLayer key="transition" /> Ensure this runs every route change
+      {/* <TransitionLayer key="transition" /> Ensure this runs every route change */}
       <div key={pathname}>
         {children}
       </div>
