f6a4b8934faa4a509b57f8c49ecff79096dde9d0
Kristina Bekher <krbekher@gmail.com>
2025-05-27 22:20:27 +0200

    About: update text

--- src/components/AboutContent.tsx
+++ src/components/AboutContent.tsx
@@ -37,10 +37,10 @@ const AboutContent = () => {
       <div className='flex flex-col-reverse lg:flex-row gap-10 items-center mb-24'>
         <div data-cursor="text" className='flex-1 text-[14px] md:text-2xl xl:text-3xl leading-tight space-y-4 lg:pt-5 lg:sticky lg:top-54 lg:self-start'>
           <p>
-            My name is Kristina Bekher, and I shoot on film.
+            Hey, I&apos;m Kristina Bekher, and I love film photography.
           </p>
           <p>
-            In my photography, I mix street, nature, architecture, and many aspects of everyday life. I like shooting anything that seems interesting and experimenting with light and color.
+            Whether it&apos;s a certain light, the feeling of a place, or the simplicity of ordinary objects — if you see it in my frame, it means the moment was too special to resist shooting.
           </p>
           <p>
             By day, I&apos;m a software developer driving on-site experimentation at Douglas.
@@ -92,64 +92,41 @@ const AboutContent = () => {
             <h2 className="text-3xl md:text-6xl mb-6">Gear</h2>
 
             <p>
-              My first ever film camera was an old rangefinder, the FED 5. I started with the hard artillery, so to speak, slowly switching to easier-to-use SLRs.
+              My first ever film camera was an old rangefinder, the FED 5. I started with the hard artillery, so to speak, slowly switching o, in my opinion, easier-to-use SLRs.
             </p>
             <p>
-              In 2022, a whole new world opened up to me thanks to the Japanese Minolta SR-1s. That was when I realized the older cameras I&apos;d been using did have their limits. That same year, Asahi Optical announced the Pentax 17, and I&apos;d been looking forward to it ever since.
+              In 2022, a whole new world opened up to me thanks to the Japanese Minolta SR-1s. That was when I realized the older cameras I&apos;d been using did have their limits.
             </p>
             <p>
-              When Pentax 17 finally came out, I soon found myself unboxing my first-ever new and half-frame film camera. I was a bit suspicios about it at first, mainly because I prefer full manual control. But Pentax still lets me experiment with exposure steps, modes, and focus, which is quite tricky.
+              When Pentax 17 finally came out, I soon found myself unboxing my first-ever new film camera. I was a bit suspicios about it at first, but Pentax does have room for experiment with its exposure steps and modes. Shooting full 35mm after a half frame feels like shooting medium size (which I haven&apos;t tried yet).
             </p>
             <p>
-              Canon AE-1 is the most recent addition to my kit. With its automatic aperture exposure, I worry less about missing moments. It&apos;s paired with a Rolev M.G. 55mm Skylight filter on the lens. I absolutely love the images I get, and I hope you&apos;ll enjoy them as much.
+              Canon AE-1 with a Rolev M.G. 55mm Skylight filter on the lens is the most recent addition to my kit. I may worry less now about missing moments thank to the built-in automatic aperture exposure.
+            </p>
+            <p className='text-[14px] color-[var(--accent)]'>
+              Note: All images were developed and scanned at <Link
+                href="https://fotovramci.com/?srsltid=AfmBOopLTn3khuFhn_KLyzZV3vapYgKymv51BY5a4aphVes0bVGFeqq5"
+                aria-label="Visit Foto v Ramci — a film developing and scanning lab"
+                className='font-mono custom-transition hover:text-[var(--accent)]'
+                target="blank"
+                data-cursor="text"
+              >
+                Fotovramci
+              </Link> and <Link
+                href="https://filmspeedlab.com/?srsltid=AfmBOop1F_0ZJmwpr82_93Ie-vRs8FcVmjtJMhgRESK4FCWs0t4_tkCg"
+                aria-label="Visit Film Speed Lab — a film developing and scanning lab"
+                className='font-mono custom-transition uppercase hover:text-[var(--accent)]'
+                target="blank"
+                data-cursor="text"
+              >
+                Film Speed Lab
+              </Link>.
             </p>
           </div>
         </div>
       </div>
 
       {/* // TODO: Selected Galleries */}
-
-      <div className="mb-24">
-        <div className="flex flex-col lg:grid lg:grid-cols-2 gap-10">
-          <div>
-            <h2 className="text-3xl md:text-4xl mb-6 uppercase">Where Film Comes to LIfe</h2>
-
-            <p className='mb-6'>All the images you see here were developed and scanned by Foto v Ramci, located in Kyiv, and Film Speed Lab — a Kyiv-hearted film lab based in Berlin. If you shoot film, you should definitely give them a try!</p>
-
-            <div className='flex flex-col gap-4 text-2xl'>
-              <Link
-                href="https://fotovramci.com/?srsltid=AfmBOopLTn3khuFhn_KLyzZV3vapYgKymv51BY5a4aphVes0bVGFeqq5"
-                aria-label="Visit Foto v Ramci — a film developing and scanning lab"
-                className="uppercase"
-                target="blank"
-                data-cursor="text"
-              >
-                Fotovramci
-              </Link>
-              <Link
-                href="https://filmspeedlab.com/?srsltid=AfmBOop1F_0ZJmwpr82_93Ie-vRs8FcVmjtJMhgRESK4FCWs0t4_tkCg"
-                aria-label="Visit Film Speed Lab — a film developing and scanning lab"
-                className="uppercase"
-                target="blank"
-                data-cursor="text"
-              >
-                FILM.SPEED.LAB
-              </Link>
-            </div>
-          </div>
-
-          {/* <div className='flex-1 w-full lg:max-w-[600px] m-auto lg:m-0'>
-            <Image
-              src="https://d14lj85n4pdzvr.cloudfront.net/fsl.jpg?w=600&format=webp&cache=31536000"
-              alt='Foto v Ramci Crew'
-              width={300}
-              height={300}
-              className='w-full h-auto object-cover grayscale'
-            />
-          </div> */}
-
-        </div>
-      </div>
 
     </section>
   );
