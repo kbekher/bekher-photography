5038724d993100e2cd0eea386af90d67d0f74ad3
Kristina Bekher <krbekher@gmail.com>
2025-07-09 07:11:43 +0200

    Update meta tags

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -41,7 +41,10 @@ export function Head() {
       <link rel="shortcut icon" href="/favicon.ico" />
       <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
       <meta name="apple-mobile-web-app-title" content="Kristina Bekher | Film Photography Gallery" />
+      <meta name="apple-mobile-web-app-capable" content="yes" />
+      <meta name="apple-mobile-web-app-status-bar-style" content="default" />
       <link rel="manifest" href="/site.webmanifest" />
+      <meta name="theme-color" content="#cbcbcf" />
     </>
   );
 }
